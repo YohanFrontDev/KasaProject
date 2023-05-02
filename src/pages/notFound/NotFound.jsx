269d0e80613261536fe01c43ebb405829c91ef26
Yohan Ferreira <yohan.dev@outlook.fr>
2023-05-02 12:48:36 +0200

    last commit

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -10,7 +10,7 @@ export default function NotFound() {
 		<div className='notFound'>
 			<div className="notFound_infos">
 				<h1 className='notFound_infos_title'>404</h1>
-				<p className='notFound_infos_content'>Oups ! La page que vous recherchez n'existe pas.</p>
+				<p className='notFound_infos_content'>Oups ! La page que vous demandez n'existe pas.</p>
 			</div>
 			<Link className='notFound_infos_return' to='/'>Retourner sur la page d'accueil</Link>
 		</div>
